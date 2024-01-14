73e22f1a487ffd6aa315c1478a5f5fabcc2d09d5
Gabriel Nascimento <gabrielnascimento2045@gmail.com>
2024-01-14 20:00:36 -0300

    CONFIGURADO ROTA COMP HOME / CONFIGURADO PAPEL DE PAREDE TELA LOGIN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = []
+{
+  path: ''
+  component: HomeComponent //criado rota entre o component home
+}
+;
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
