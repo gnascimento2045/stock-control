56f71423ae2cfa0a2847953929f31f24c7d96dea
Gabriel Nascimento <gabrielnascimento2045@gmail.com>
2024-01-14 21:31:31 -0300

    Criado pasta de importação de cadastros

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { enviroment } from 'src/enviroments/enviroment';
+import { SignupUserRequest } from './../../models/user/SignupUserRequest';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-
+  private API_URL = enviroment.API_URL;
 
   constructor(private http: HttpClient) { }
+
+  signupUser(){requestDatas: SignupUserRequest}
 }
