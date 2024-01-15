8a83c461b27a38b920b93c7aa7f5a3ff145d865f
Gabriel Nascimento <gabrielnascimento2045@gmail.com>
2024-01-14 21:13:23 -0300

    Criado validação do formulario e exportação dos dados preenchidos no form

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email:['',Validators.required],
+    password:['',Validators.required]
+  })
+
+  signupForm = this.formBuilder.group({
+    name:['',Validators.required],
+    email:['',Validators.required],
+    password:['',Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder){}
+
+  onSubmitLoginForm(): void {
+      console.log('DADOS DO FORMULÁRIO DE LOGIN', this.loginForm.value);
+  };
+
+  onSubmitSignUpForm(): void {
+    console.log('DADOS DO FORMULÁRIO DE LOGIN', this.signupForm.value);
+};
 }
